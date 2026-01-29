cab766a1e5297a7073816d98c2c6216422d7ffb8
parthbhavsar1 <parth.bhavsar@simformsolutions.com>
2026-01-29 15:03:55 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const PORT = process.env.PORT ?? 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello From The Server!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
