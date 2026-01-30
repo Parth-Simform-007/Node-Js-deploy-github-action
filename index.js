e21f06430090160ea86ea2b23c4e558ad65c9f2b
parthbhavsar1 <parth.bhavsar@simformsolutions.com>
2026-01-30 14:40:34 +0530

    checking index file

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT ?? 3000;
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello From The Server! level-------1');
+    res.send('Hello From The Server! level-------1 WOOO');
 });
 
 app.listen(PORT, () => {
